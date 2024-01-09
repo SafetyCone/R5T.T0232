270066796ce7d6cc45904f1e9941c520897f9828
davidcoats <david@getsafetycone.com>
2024-01-09 14:47:32 -0800

    Initial commit.

---
+++ source/R5T.T0232.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0232.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.T0232/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0232
+{
+	/// <summary>
+	/// Platform, catch-all, location-aware types for the netstandard2.1 platform.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
